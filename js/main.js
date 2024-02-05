0b5fb20db52d2d332239940176a35e98ab97684f
JuanPabloCardenasGonzalez <jpcardenasgonzalez@gmail.com>
2024-02-05 00:42:39 -0500

    feat: :sparkles: Add shopping cart functionality to index.html
    This commit includes the following changes:
    
    In 'index.html':
    - Added a button to display the products added to the shopping cart.
    - Created containers for both added products and products to be added.
    - Included star icons for product rating.
    
    In 'js/main.js':
    - Implemented functions to display the mentioned containers.
    
    @CampusLands

--- js/main.js
+++ js/main.js
@@ -23,3 +23,47 @@ function seleccionarMarca(event) {
         }
     });
 }
+
+function obtenerProducto(event) {
+
+    const tarjetaActual = event.currentTarget.closest('.card');
+    const card = tarjetaActual.cloneNode(true);
+
+    const botonCerrar = document.createElement('button');
+    botonCerrar.innerHTML = '<i class="bx bx-x"></i>';
+    botonCerrar.onclick = () => {
+        cerrarAgregarProducto();
+    };
+
+    const imagenes = card.querySelector(".imagenes");
+    const borrarImagenes = imagenes.querySelector(".listaImagenes");
+    imagenes.removeChild(borrarImagenes);
+    imagenes.appendChild(botonCerrar);
+
+    const footCard = card.querySelector('.footCard');
+    const boton = card.querySelector('#obtener');
+    boton.innerHTML = 'Agregar';
+    const input = document.createElement('input');
+    footCard.appendChild(input);
+
+
+    const agregarProducto = document.getElementById('agregarProducto');
+    agregarProducto.innerHTML = '';
+    agregarProducto.appendChild(card);
+    agregarProducto.style.display = "block";
+}
+
+function cerrarAgregarProducto() {
+    const agregarProducto = document.getElementById('agregarProducto');
+    agregarProducto.style.display = "none";
+}
+
+function mostrarProductosAgregados() {
+    const productosAgregados = document.getElementById("productosAgregados");
+    productosAgregados.style.display = "flex";
+}
+
+function cerrarProductosAgregados() {
+    const productosAgregados = document.getElementById("productosAgregados");
+    productosAgregados.style.display = "none";
+}
